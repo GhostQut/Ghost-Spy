ce43670a8d6d0360dab503c5de6b6e168386b753
GhostQut <69810133+GhostQut@users.noreply.github.com>
2023-12-09 23:59:24 +0300

    Update PetSim99.lua

--- PetSim99.lua
+++ PetSim99.lua
@@ -42,28 +42,6 @@ local function contains(table, val)
    return false
 end
 
-local e = Instance.new("BillboardGui",v)
-        e.Name = "ESP"
-        e.Size = UDim2.new(5,0, 5,0)
-        e.AlwaysOnTop = true
-local s = Instance.new("Frame",a)
-        s.Size = UDim2.new(1,0, 1,0)
-        s.BackgroundTransparency = 0.80
-        s.BorderSizePixel = 0
-        s.BackgroundColor3 = tcolor
-local p = Instance.new('TextLabel',b)
-        p.Size = UDim2.new(1,0,1,1)
-        p.BorderSizePixel = 0
-        p.TextSize = 17
-        p.Text = "Chest"
-        p.BackgroundTransparency = 1
-        p.TextColor3 = tcolor
-        p.TextStrokeColor3 = Color3.fromRGB(6, 6, 6)
-        p.TextStrokeTransparency = 0.7
-    end
-end
-end
-
 local machines = {
     {"PotionVendingMachine1";"Cherry Blossom"};
     {"PotionVendingMachine2";"Safari"};
@@ -185,7 +163,7 @@ end)
 
 tab4.newToggle("ESP DigSite", "", false, function(toggleState)
    if toggleState == true then
-      while true do
+while true do
 task.wait()
 for __,v in pairs(game.Workspace["__THINGS"].__INSTANCE_CONTAINER.Active.Digsite.Important.ActiveChests:GetChildren()) do
     if v:FindFirstChild("ESP") then v:FindFirstChild("ESP"):Destroy() end
@@ -193,7 +171,28 @@ for __,v in pairs(game.Workspace["__THINGS"].__INSTANCE_CONTAINER.Active.Digsite
         if v.Name == "Animated" then
             tcolor = Color3.fromRGB(222, 184, 135)
         end
-   end
+
+        local a = Instance.new("BillboardGui",v)
+        a.Name = "ESP"
+        a.Size = UDim2.new(5,0, 5,0)
+        a.AlwaysOnTop = true
+        local b = Instance.new("Frame",a)
+        b.Size = UDim2.new(1,0, 1,0)
+        b.BackgroundTransparency = 0.80
+        b.BorderSizePixel = 0
+        b.BackgroundColor3 = tcolor
+        local c = Instance.new('TextLabel',b)
+        c.Size = UDim2.new(1,0,1,1)
+        c.BorderSizePixel = 0
+        c.TextSize = 17
+        c.Text = "Chest"
+        c.BackgroundTransparency = 1
+        c.TextColor3 = tcolor
+        c.TextStrokeColor3 = Color3.fromRGB(6, 6, 6)
+        c.TextStrokeTransparency = 0.7
+    end
+end
+end
     else
         toggleState = false
     end
