e4685eaffe3c178c75f8c0c2213b1d5ba197868a
GhostQut <69810133+GhostQut@users.noreply.github.com>
2024-04-17 21:05:09 +0300

    Update RoGhoul.lua

--- RoGhoul.lua
+++ RoGhoul.lua
@@ -20,7 +20,7 @@ repeat wait() until player:FindFirstChild("PlayerFolder")
 local team, remotes, stat = player.PlayerFolder.Customization.Team.Value, get.ReplicatedStorage.Remotes, player.PlayerFolder.StatsFunction
 local oldtick, farmtick = 0, 0
 local camera = workspace.CurrentCamera
-local myData = loadstring(game:HttpGet("https://raw.githubusercontent.com/z4gs/scripts/master/Settings.lua"))()("Ro-Ghoul Autofarm", {
+local myData = {
 	Skills = {
 		E = false,
 		F = false,
@@ -40,7 +40,7 @@ local myData = loadstring(game:HttpGet("https://raw.githubusercontent.com/z4gs/s
 	ReputationFarm = false,
 	ReputationCashout = false,
 	AutoKickWhitelist = ""
-})
+}
 
 local array = {
 	boss = {
