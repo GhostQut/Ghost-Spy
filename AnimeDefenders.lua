95ada3f9bc39fffced1b325953d2f5aa3167ea4c
GhostQut <69810133+GhostQut@users.noreply.github.com>
2024-06-16 16:19:42 +0300

    n

---
+++ AnimeDefenders.lua
@@ -0,0 +1,13 @@
+local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostQut/FreemUI-V2/main/Source.lua"))()
+
+local window = DrRayLibrary:Load("Anime Defenders", "Default")
+
+local tab1 = DrRayLibrary.newTab("Main", "")
+local tab2 = DrRayLibrary.newTab("SetUp", "")
+local tab3 = DrRayLibrary.newTab("Summon", "")
+local tab4 = DrRayLibrary.newTab("Misc", "")
+local tab6 = DrRayLibrary.newTab("Other", "")
+
+tab3.newButton("Redeem ALL Codes", "", function()
+	
+end)
