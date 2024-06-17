cdb7b32a16d31d2930c6098f1d776f20d8e4ac7b
GhostQut <69810133+GhostQut@users.noreply.github.com>
2024-06-17 15:17:49 +0300

    m

--- ADG.lua
+++ ADG.lua
@@ -4,7 +4,6 @@ local window = DrRayLibrary:Load("Anime Defenders", "Default")
 
 local tab1 = DrRayLibrary.newTab("Main", "")
 local tab2 = DrRayLibrary.newTab("Macro", "")
-local tab3 = DrRayLibrary.newTab("Summon", "")
+local tab3 = DrRayLibrary.newTab("AutoPlay+", "")
 local tab4 = DrRayLibrary.newTab("Misc", "")
-local tab5 = DrRayLibrary.newTab("Other", "")
 
