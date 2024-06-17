632c3ceeacfbe29fc0a20e0e16ad78aacd128be7
GhostQut <69810133+GhostQut@users.noreply.github.com>
2024-06-17 15:15:02 +0300

    mm

---
+++ ADM.lua
@@ -0,0 +1,18 @@
+local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostQut/FreemUI-V2/main/Source.lua"))()
+
+local window = DrRayLibrary:Load("Anime Defenders", "Default")
+
+local tab1 = DrRayLibrary.newTab("Main", "")
+local tab2 = DrRayLibrary.newTab("Macro", "")
+local tab3 = DrRayLibrary.newTab("Summon", "")
+local tab4 = DrRayLibrary.newTab("Misc", "")
+local tab5 = DrRayLibrary.newTab("Other", "")
+
+local codes = {"update2", "idk", "thanks500k", "MEMBEREREBREWRERES", "raidsarecool", "sorry4delay", "dayum100m", "wsindach4ht", "200kholymoly", "adontop", "subcool", "sub2toadboigaming", "sub2jonaslyz", "sub2riktime", "sub2mozking", "sub2karizmaqt", "sub2nagblox"}
+
+tab5.newButton("Redeem ALL Codes", "", function()
+    for i, code in pairs(codes) do
+        task.wait(.1)
+        game:GetService("ReplicatedStorage").Remotes.UseCode:InvokeServer(code)
+    end
+end)
